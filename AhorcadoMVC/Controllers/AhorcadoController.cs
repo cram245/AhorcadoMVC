258a2ed4dd0b0abd9c8dddfc7ba952915d64ce6f
cram245 <marcalbaf3@gmail.com>
2024-08-07 11:57:32 +0200

    Agregar archivos de proyecto.

---
+++ AhorcadoMVC/Controllers/AhorcadoController.cs
@@ -0,0 +1,50 @@
+﻿using AhorcadoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhorcadoMVC.Controllers
+{
+    public class AhorcadoController : Controller
+    {
+        private static Hangman _game;
+
+        public IActionResult Ahorcado()
+        {
+            if (_game == null)
+            {
+                return RedirectToAction("Start");
+            }
+
+            return View(_game);
+        }
+
+        [HttpGet]
+        public IActionResult Start()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Start(string wordToGuess)
+        {
+            _game = new Hangman(wordToGuess);
+            return RedirectToAction("Ahorcado");
+        }
+
+        [HttpPost]
+        public IActionResult Guess(char letter)
+        {
+            _game.Guess(letter);
+
+            if (_game.IsGameWon())
+            {
+                ViewBag.Message = "You won!";
+            }
+            else if (_game.IsGameLost())
+            {
+                ViewBag.Message = "You lost! The word was " + _game.WordToGuess;
+            }
+
+            return View("Ahorcado", _game);
+        }
+    }
+}
---
+++ AhorcadoMVC/Models/Hangman.cs
@@ -0,0 +1,62 @@
+﻿namespace AhorcadoMVC.Models
+{
+    public class Hangman
+    {
+        public string WordToGuess { get; set; }
+        public string CurrentGuess { get; set; }
+        public int AttemptsLeft { get; set; }
+        public List<char> GuessedLetters { get; set; }
+
+        public Hangman(string word)
+        {
+            WordToGuess = word;
+            CurrentGuess = new string('_', word.Length);
+            AttemptsLeft = 6; // Número típico de intentos en el ahorcado
+            GuessedLetters = new List<char>();
+        }
+
+        public bool Guess(char letter)
+        {
+            if (GuessedLetters.Contains(letter))
+            {
+                return false;
+            }
+
+            GuessedLetters.Add(letter);
+
+            if (WordToGuess.Contains(letter))
+            {
+                var updatedGuess = new char[WordToGuess.Length];
+                for (int i = 0; i < WordToGuess.Length; i++)
+                {
+                    if (WordToGuess[i] == letter)
+                    {
+                        updatedGuess[i] = letter;
+                    }
+                    else
+                    {
+                        updatedGuess[i] = CurrentGuess[i];
+                    }
+                }
+                CurrentGuess = new string(updatedGuess);
+                return true;
+            }
+            else
+            {
+                AttemptsLeft--;
+                return false;
+            }
+        }
+
+        public bool IsGameWon()
+        {
+            return !CurrentGuess.Contains('_');
+        }
+
+        public bool IsGameLost()
+        {
+            return AttemptsLeft <= 0;
+        }
+    }
+
+}
